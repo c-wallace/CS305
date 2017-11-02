8e19f004a81b3cb39b11932b5e1001fe96c350ec
Wallace <wallace185@live.marshall.edu>
2017-11-02 11:18:25 -0700

    changes to label names and css

--- CS305_WebApp/Models/RosterModel.cs
+++ CS305_WebApp/Models/RosterModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,11 @@ namespace CS305_WebApp.Models
     public class RosterModel
     {
         public int ID { get; set; }
+        [Display(Name = "Last Name")]
         public string Lastname { get; set; }
+        [Display(Name = "First Name")]
         public string Firstname { get; set; }
+        [Display(Name = "Description")]
         public string description { get; set; }
     }
 }
